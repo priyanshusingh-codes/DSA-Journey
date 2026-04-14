44dc5a1c634f1bd438800c7c4ddeb0250f90c960
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-14 19:19:03 +0530

    DSA JOURNEY

---
+++ Lecture-05-Functions/Function.cpp
@@ -0,0 +1,18 @@
+/*
+A Function is a block of code that performs a specific task. 
+It is a reusable piece of code that can be called multiple times in a program. 
+Functions help to break down a complex problem into smaller, more manageable parts, making the code easier to read and maintain.
+*/
+
+
+//Two Types of Function in C++
+// Return Type Function and  Member Function
+
+
+//General Syntax of a Function in C++
+/*
+return_type function_name(parameters) {
+    // body
+    return value;
+}
+*/
---
+++ Lecture-05-Functions/Function_Syntax.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Function Determination
+void PrintHello() 
+{
+    cout <<"Hello\n";
+}
+int main()
+{
+    //Fuinction Invoke/ Calling a Function
+    PrintHello();
+    return 0;   
+}
