c0dbc3d8ba4d22840d1d239a4fb6ef2126fe20cc
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 23:23:22 +0530

    Global_Scope.cpp

---
+++ Lecture-07-Bitwise-Operator/Block_Scope.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if (true) {
+        int y = 5;
+        cout << y;
+    }
+
+    // cout << y; ❌ ERROR
+}
+// y dies outside the block
