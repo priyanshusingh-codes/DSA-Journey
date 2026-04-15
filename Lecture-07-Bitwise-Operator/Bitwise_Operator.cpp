ffd2fdb1cab8cb8512fc6f6aa9a65a9cc1b6d881
Priyanshu Singh <mepriyanshu6@gmail.com>
2026-04-15 22:53:03 +0530

    Bitwise_Operator.cpp

---
+++ Lecture-07-Bitwise-Operator/Bitwise_Operator.cpp
@@ -0,0 +1,73 @@
+//Bitwise Operator performs on Binary
+/*
+Bitwise &  (AND)
+--------------
+0 & 0 => 0
+0 & 1 => 0
+1 & 0 => 0
+1 & 1 => 1
+
+
+Bitwise | (OR)
+---------------
+
+o | o => 0
+1 | 1 => 1
+1 | 0 => 1
+1 | 1 => 1
+
+Bitwise ^ (XOR = Exclusive OR)
+-----------------------------
+
+same => 0
+different => 1
+
+0 ^ 0 => 1
+1 ^ 1 => 0
+0 ^ 1 => 1
+1 ^ 0 => 1 
+
+example : 
+4 = 0100
+8 = 1000
+
+4^8 => 0100
+       1000
+       -----
+       1100 12 base 10
+
+    Bitwise << //Works internally on memory
+    = left shift
+    ans = a * 2^b
+
+    n << i // n shifted to i place 
+
+    example :
+     4 = 100 
+     n<<1
+     100_
+
+     Bitwise >> //Works internally on memory
+     = Right Shift
+
+    and = a/2^b
+example :
+    n = 10  1010
+            0101 = 101 = 5 base 10
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
