e5065cd134d5511c4806e3926583916c04018233
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-04-29 23:16:48 +0530

    vectors_syntax1.cpp

--- Lecture-09-VECTORS/vectors_syntax1.cpp
+++ Lecture-09-VECTORS/vectors_syntax1.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
-#include<vector>
+#include<vector>//means you are importing the C++ Standard Library vector container.
 using namespace std;
 int main()
 {
-    vector<int>vec = {1, 2, 3};// size = 0 by default
-    cout << vec[0, 1, 2];
+    vector<int>vec = {1, 2, 3};// size = 0 by default if not arguments declared
+    cout << vec[0];
     return 0;
 }
