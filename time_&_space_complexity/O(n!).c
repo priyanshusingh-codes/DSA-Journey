77c7cf3557ec286dc233e9ed290c07a829ac4578
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-02 00:16:10 +0530

    O(n!).c

---
+++ time_&_space_complexity/O(n!).c
@@ -0,0 +1,42 @@
+/*
+
+O(n!) means runtime grows like the factorial of n:
+n! = n × (n-1) × (n-2) × ... × 2 × 1
+So each increase in n causes an explosion in possibilities.
+
+
+Time
+ ^
+ |                           *
+ |                      *
+ |                 *
+ |            *
+ |        *
+ |    *
+ | *
+ |____________________> n
+
+
+ Real Problems with O(n!)
+Traveling Salesman (Brute Force)
+Try every city visiting order.
+Numbers/letters arrangement
+Try all permutations.
+Scheduling brute force
+Check every possible order
+*/
+
+
+/*
+
+Full Ranking (best to worst)
+O(1)
+O(log n)
+O(n)
+O(n log n)
+O(n²)
+O(n³)
+O(2^n)
+O(n!)
+
+*/
