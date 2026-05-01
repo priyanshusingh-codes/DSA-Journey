1b095c9df7c43a90a7fcb9cf16c8ca008acd1a96
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-02 02:15:55 +0530

    max_subarray_sum.cpp

---
+++ Lecture-10-Subarray/max_subarray_sum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int main()
+{
+
+    int n =5;
+    int arr[5] = {1,2,3,4,5};
+    int maxsum = INT_MIN;  // Smallest possible integer initially
+
+       // Select starting index of subarray
+    for (int st = 0; st < n; st++){
+
+        int currsum=0;// Reset sum for each new start index
+
+           // Select ending index of subarray
+        for(int end = st; end < n; end++){
+
+            currsum += arr[end]; // Add current ending element
+
+            maxsum = max(currsum, maxsum);// Update maximum sum
+        }
+    }
+    cout << "max subarray sum = "<< maxsum<<endl;
+    return 0;
+}
