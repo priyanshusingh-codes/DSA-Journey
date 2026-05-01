9fd4491587d1f7d46998e79744865d0816a3bc6a
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-01 23:27:44 +0530

    O(1).c

---
+++ time_&_space_complexity/O(1).c
@@ -0,0 +1,24 @@
+/*
+
+O(1) means constant time complexity.
+That means the operation takes the same amount of time, no matter how big the input gets.
+
+Example 1: Array Access
+int arr[] = {5, 8, 2, 9};
+cout << arr[2];
+
+Getting arr[2] directly uses indexing.
+Computer calculates memory location instantly. => ✅ O(1)
+Because it doesn't matter if array size is 4 or 4 million.
+
+
+
+gRAPH
+
+Time
+ ^
+ |-------------------O(1)
+ |
+ |
+ |____________________> n
+*/
