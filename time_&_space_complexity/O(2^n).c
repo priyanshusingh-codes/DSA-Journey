c7db9d07e7b63d6395525540dafc2e52b0aa11c8
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-02 00:13:39 +0530

    O(2^n).c

---
+++ time_&_space_complexity/O(2^n).c
@@ -0,0 +1,26 @@
+/*
+
+O(2^n) means runtime doubles every time input n increases by 1.
+That’s exponential time complexity.
+
+Time
+ ^
+ |                         *
+ |                    *
+ |               *
+ |          *
+ |      *
+ |   *
+ | *
+ |____________________> n
+
+
+
+
+ Where You See O(2^n)
+Recursive brute force
+Subset generation
+Backtracking without pruning
+Naive DP problems
+Some game state searches
+*/
