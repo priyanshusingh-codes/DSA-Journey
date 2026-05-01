eb828343f7078c832eba4197423587d91200227d
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-01 23:17:56 +0530

    time_complexity.c

---
+++ time_&_space_complexity/time_complexity.c
@@ -0,0 +1,44 @@
+/*
+
+Time complexity => Amount of time taken as function of input size(n)
+
+ops vs input size(n) 
+f(x) = x
+f(n) = n <=> O(n)
+
+//checked for worst case scenario.
+//comare two algorithms.
+
+*/
+
+
+/*
+
+Big O Notation : 
+
+O(n) 
+=> worst case scenario/complexity
+=> upper bound
+
+Rules :
+i) ignore constant
+ii)largest form
+
+
+eg: f(n)= 4n^2 + 3^n + 5, n=10^5
+=> n^2 + n + 1
+=> n^2 => O(n^2) => time complexity
+
+*/
+
+
+
+/*
+
+Worst case : O (upper bound)  **
+
+average case : 0 
+
+best case : Ω (lower bound)
+
+*/
