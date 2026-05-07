14de54fbaf3e4309df76e951b469db1c72d9bf49
Priyanshu SIngh <mepriyanshu6@gmail.com>
2026-05-07 22:48:26 +0530

    vectors_syntax2.cpp

--- Lecture-09-VECTORS/vectors_syntax2.cpp
+++ Lecture-09-VECTORS/vectors_syntax2.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
-#include<vector>means you are importing the C++ Standard Library vector container.
+#include<vector>//means you are importing the C++ Standard Library vector container.
 using namespace std;
 int main()
 {
-    vector<int>vec(5,0); // 5 is size of vector and o is index value for eachh vector
+    vector<int>vec(5,0); // 5 is size of vector and 0 is index value for eachh vector
     cout << vec [0] << endl;
     cout << vec [1] << endl;
     cout << vec [2] << endl;
